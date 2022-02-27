8936dc67ce2fa250a043dbde46282ef26f4de6ec
unknown <nikhil98patil@gmail.com>
2022-02-28 00:02:35 +0530

    Added: UC3 for Binary Search of Key

---
+++ UC3_BST/MyBinaryNode.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.bst;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+
+	public 	MyBinaryNode(K key) {
+		this.key=key;
+		this.left=null;
+		this.right=null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+}
+
+
